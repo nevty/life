8424294af83d58d7629f02b456a8f83c0c688fb2
nevty <theacutemace@gmail.com>
2023-11-02 13:50:01 +0300

    cell and grid ui components

---
+++ src/molecules/grid/ui.tsx
@@ -0,0 +1,70 @@
+import Konva from "konva"
+import { Group, Layer, Line, Rect } from "react-konva"
+import { LifeCell } from "../../atoms/cell"
+
+const CELLS_OUTLINE_COLOR = '#171717';
+
+const CELLS_OUTLINE_CONFIG: Konva.LineConfig = {
+	stroke: CELLS_OUTLINE_COLOR,
+	strokeWidth: 1
+}
+
+type GridPros = {
+	width: number
+	height: number
+	cols: number
+	rows: number
+	cellSize: number
+	cells: number[]
+}
+
+export const Grid = ({ width, height, cols, rows, cellSize, cells }: GridPros) => {
+	const cellOutlinesX = new Array(cols).fill(CELLS_OUTLINE_CONFIG)
+	const cellOutlinesY = new Array(rows).fill(CELLS_OUTLINE_CONFIG)
+
+	return (
+		<Layer listening={false} >
+			<Group>
+				{cells.map((order, i) => {
+					const x = (order % cols) * cellSize
+					const y = Math.floor(order / cols) * cellSize
+					return (
+						<LifeCell
+							key={i}
+							y={y}
+							x={x}
+							width={cellSize}
+							height={cellSize}
+						/>
+					)
+				})}
+			</Group>
+			<Rect
+				x={1}
+				y={1}
+				width={width - 1}
+				height={height - 1}
+				stroke={CELLS_OUTLINE_COLOR}
+				strokeWidth={1}
+			/>
+			{cellOutlinesY.map((l, i) => (
+				<Line
+					key={i}
+					{...l}
+					y={cellSize * i}
+					points={[0, 0, width, 0]}
+				/>
+			)
+			)}
+			{cellOutlinesX.map((l, i) => (
+				<Line
+					key={i}
+					{...l}
+					x={cellSize * i}
+					points={[0, 0, 0, height]}
+				/>
+			)
+			)}
+		</Layer>
+	)
+}
